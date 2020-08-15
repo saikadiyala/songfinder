90b333f3e23cf0b8b7d089af0fe614bda7d0f3b2
sai kadiyala <saikadiyala201@gmail.com>
2020-08-15 23:42:12 +0530

    Updates

---
+++ precache-manifest.5be3822f1d08609c08d78841d424b0fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d91383f67f04a4b3f09553bc113f2645",
+    "url": "/songfinder/index.html"
+  },
+  {
+    "revision": "29a72548031fbed2a849",
+    "url": "/songfinder/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0981be069cfa4d3fda07",
+    "url": "/songfinder/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "29a72548031fbed2a849",
+    "url": "/songfinder/static/js/2.f7d55251.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/songfinder/static/js/2.f7d55251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0981be069cfa4d3fda07",
+    "url": "/songfinder/static/js/main.fad1e19a.chunk.js"
+  },
+  {
+    "revision": "d76652ba4ad0e47129f6",
+    "url": "/songfinder/static/js/runtime-main.1e394783.js"
+  }
+]);
